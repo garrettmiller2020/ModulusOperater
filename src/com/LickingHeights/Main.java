69cf44bc6620ff81ee62339b948f7a3597a41eae
Garrett Miller <garrett.miller.2020@student.lhschools.org>
2020-02-19 13:52:16 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,19 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) {testOddness();}
+
+    public static void testOddness() {
+       Scanner Keyboard;
+       Keyboard = new Scanner(System.in);
+       System.out.println("Enter positive number");
+       int number = Keyboard.nextInt();
+       System.out.println("Number is even: " + (number % 2 ==0));
+       System.out.println("Number is odd: " + (number % 2 !=0));
+
+
     }
 }
